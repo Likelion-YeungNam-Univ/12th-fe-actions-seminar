3d29609ada3702649254b60421d46e4d8a81ee1a
Jinwoo Lee <binaryWooo@gmail.com>
2024-07-05 01:06:26 +0900

    feat: edit text and link

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>멋쟁이사자처럼 영남대학교 12기 이진우</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://jinlee.kr/"
           target="_blank"
           rel="noopener noreferrer"
         >
