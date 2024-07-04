e08a18c3880fba8e85b939bdbc88bb74968aae32
Jinwoo Lee <binaryWooo@gmail.com>
2024-07-05 01:07:31 +0900

    feat: edit text and link

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>멋쟁이사자처럼 영남대학교 12기 이진우</p>
+        <p>멋쟁이사자처럼 영남대학교 12기 대표 이진우</p>
         <a
           className="App-link"
           href="https://jinlee.kr/"
